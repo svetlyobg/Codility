8d050a904601f3fc2cf6cafc432d666c75d57c60
svetlyobg <swk_12@abv.bg>
2017-07-14 12:58:16 +0300

    Lesson-2-Arrays
    https://codility.com/programmers/lessons/2-arrays/
    
    CyclicRotation
    OddOccurrencesInArray

---
+++ Arrays/x/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//https://codility.com/programmers/lessons/2-arrays/
+namespace x
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+			if (A.Length == 0) return A;
+			K = K % A.Length; 
+			var result = new int[A.Length];
+			Array.Copy(A, A.Length - K, result, 0, K);
+			Array.Copy(A, 0, result, K, A.Length - K);
+			return result;
+		}
+	}
+}
+
